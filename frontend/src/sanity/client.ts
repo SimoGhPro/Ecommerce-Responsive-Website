035f5ef743d1cf698b01ff587f8ff989fe26b0e6
SimoGhPro <medghlahpro@gmail.com>
2025-08-19 17:32:48 +0100

    Security

--- frontend/src/sanity/client.ts
+++ frontend/src/sanity/client.ts
@@ -1,9 +1,9 @@
 import { createClient } from 'next-sanity';
 
 export const client = createClient({
-    projectId: '8yq0hl58', 
-    dataset: 'jolof_data',
+    projectId: process.env.SANITY_STUDIO_PROJECT_ID, 
+    dataset: process.env.SANITY_STUDIO_DATASET,
     apiVersion: '2025-08-04', 
     useCdn: false,    
-    token: 'skUnDsoH0fYKEzEGlerDLJsn4ztv3i5duHephjg9by9JdaXe7ZU0xZgxGJx8VT9WbUIL0H3Y8wSG9dtorZ0ixmXgDArndSJXZaG5JmqEp3Wwb5zpbonLII4bwTYnI3LHqL72bu7bxBqYHOZHLAMgo7cD9qsh7DUzTtixVtpF6Q5mKJNMdzSB'
+    token: process.env.SANITY_API_TOKEN
 });
